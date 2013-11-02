dc763372498c36dbb71438e2a291dd692b8d4642
magomimmo <mimmo.cosenza@gmail.com>
2013-11-02 12:38:06 +0100

    added hooks

--- example-projects/simple/project.clj
+++ example-projects/simple/project.clj
@@ -8,9 +8,11 @@
                  [hiccup "1.0.4"]]
   :plugins [[lein-cljsbuild "0.3.4"]
             [lein-ring "0.8.7"]]
-  :cljsbuild {
-    :builds [{:source-paths ["src-cljs"]
-              :compiler {:output-to "resources/public/js/main.js"
-                         :optimizations :whitespace
-                         :pretty-print true}}]}
+
+  :hooks [leiningen.cljsbuild]
+  
+  :cljsbuild {:builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
   :ring {:handler example.routes/app})
